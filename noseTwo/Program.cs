e6bccd9a97d70fd4cb8312bc490818b4565f075d
Matthias Alzate <matthias.alzate@elev.ga.ntig.se>
2024-11-14 13:09:20 +0100

    iohoiho

---
+++ noseTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] toys = ["Barbie", "Bouncy Balls", "Cars", "Fidget Spinners", "Bratz"];
+string[] names = ["Jasmine", "Mathtias", "Svea", "Femboy", "Markus"];
+List<string> cities = [];
+
+int[] number = [2, 2, 4, 7, 10];
+
+string answer;
+
+int exit = 0;
+
+
+for (int i = 0; i < toys.Length; i++) 
+{
+    Console.WriteLine($"{names[i]} gav {toys[i]} {number[i]} stjärnor");
+}
+
+while (exit == 0)
+{
+    Console.WriteLine("Skriv in en stad (Skriv exit om du vill ut)");
+    answer = Console.ReadLine();
+
+    if (answer == "exit") 
+    {
+        exit++;
+        cities.Remove("exit");
+    }
+    cities.Add(answer);
+}
+
+foreach (string i in cities)
+{
+    Console.WriteLine(i);
+}
+
+
+Console.ReadLine();
